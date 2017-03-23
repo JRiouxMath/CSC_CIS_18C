afb29dbb7c1716acbbbfe60a64188267e9fa2862
rcc <rcc>
2017-03-22 21:25:13 -0700

    Commit linked list from discussion

--- LinkedList - Template/src/LinkedList/LinkedList.java
+++ LinkedList - Template/src/LinkedList/LinkedList.java
@@ -1,6 +1,6 @@
 package LinkedList;
 
-public class LinkedList {
+public class LinkedList<T extends Object> {
 
     // head node
     private Node head; // beginning of linked list
@@ -11,27 +11,63 @@ public class LinkedList {
 
     // default constructor to create an empty linked list
     public LinkedList() {
-        head = new Node();
-        tail = head;
+        head = null;
+        tail = null;
         listCount = 0;
     }
 
     // add a node with the specified element to the end of this list
-    public void add(int dataValue) 
+    public void add(T dataValue) 
     {
         // to do: 3/22/17
+        Node temp = new Node(dataValue); // create our new node        
+        // if head is null, newly created node is head and tail for starters
+        if ( head == null )
+        {
+            head = temp;
+            tail = head;
+        }
+        else
+        {
+            tail.setNext(temp);
+            tail = temp;
+        }
         listCount++;
     }
 
     // inserts the specified element at the specified position in this list
-    public void add(int dataValue, int indexPosition) 
+    public void add(T dataValue, int indexPosition) 
     {
-        // to do: 3/22/17
-        listCount++;
+        // to do: 3/22/17       
+        if ( indexPosition == 0 )
+        {
+            Node temp = new Node(dataValue); // create our new node
+            temp.setNext(head);
+            head=temp;
+            listCount++;
+        }        
+        else if ( indexPosition >= listCount )
+        {
+            this.add(dataValue);
+        }
+        else
+        {
+            Node previous = null;
+            Node current = head;
+            for(int i=0;i<indexPosition;i++)
+            {
+                previous = current;
+                current = current.getNext();
+            }
+            Node temp = new Node(dataValue); // create our new node
+            previous.setNext(temp);
+            temp.setNext(current);
+            listCount++;
+        }       
     }
 
     // returns the element at the specified index position in this list
-    public int get(int indexPosition)
+    public T get(int indexPosition)
     {
         // index must be 0 or higher
         if (indexPosition < 0) {
@@ -41,12 +77,12 @@ public class LinkedList {
         Node current = head.getNext();
         for (int i = 0; i < indexPosition; i++) {
             if (current.getNext() == null) {
-                return 0;
+                return null;
             }
 
             current = current.getNext();
         }
-        return current.getData();
+        return (T)current.getData();
     }
 
     // removes the node at the specified index position in this list
@@ -65,7 +101,7 @@ public class LinkedList {
 
     // utility method to print out items in linked list
     public String toString() {
-        Node current = head.getNext();
+        Node current = head;
         String output = "";
         while (current != null) {
             output += "[" + current.getData() + "]";
@@ -76,39 +112,39 @@ public class LinkedList {
 
     // this is a private inner class, only needs to be used by the
     // public outer linkedlist class
-    private class Node {
+    private class Node<T> {
         // next node in the linked list, if null, then end of list
 
         Node next;
         // data element of the node
-        int data;
+        T data;
 
         // constructor to create node with empty value
         public Node() {
             next = null;
-            data = 0;
+            data = null;
         }
 
         // constructor to create node with a value in the node
-        public Node(int dataValue) {
+        public Node(T dataValue) {
             next = null;
             data = dataValue;
         }
 
         // constructor for if we want to specify a particular node for
         // the node to point to
-        public Node(int dataValue, Node nextNode) {
+        public Node(T dataValue, Node nextNode) {
             next = nextNode;
             data = dataValue;
         }
 
         // return the data in the node
-        public int getData() {
+        public T getData() {
             return data;
         }
 
         // set the data in the node to a value
-        public void setData(int dataValue) {
+        public void setData(T dataValue) {
             data = dataValue;
         }
 
--- LinkedList - Template/src/LinkedList/Run1000000.java
+++ LinkedList - Template/src/LinkedList/Run1000000.java
@@ -15,10 +15,15 @@ public class Run1000000 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        LinkedList test = new LinkedList();
+        LinkedList<Float> test = new LinkedList();
         // add a nodes with the dataValues of 1 to 1,000,000
-        for (int i=1;i<=1000000;i++)
-            test.add(i);
-    }
-    
+        for (int i=1;i<=10;i++)
+            test.add(i/100.0f);
+        
+        test.add(26.34534f,5);
+        test.add(99.2345f,12);
+        test.add(100.3453f,0);
+        
+        System.out.println( test.toString() );
+    }    
 }
